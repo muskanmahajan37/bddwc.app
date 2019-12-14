d20792d91e8dca2208d2aa6559e1a0fc34591cbe
Thiloshon <thiloshon@gmail.com>
2019-12-14 19:31:15 +0530

    Removing common functions

--- R/bddwc_utils.R
+++ R/bddwc_utils.R
@@ -1,59 +1,9 @@
 # bddwc Utility Functions
 
-#' @title   summarize biodiversity dataframe
-#' @description  Summary table function.
+#' @title   get representation of bddwc dictionary that can be edited by hand
+#' @description  Get text version of dictionary to show in edit tab.
 #'
-#' @param data biodiversity dataframe
-#'
-#' @keywords internal
-#' @export
-summarizeDataframe <- function(data) {
-    if (nrow(data) == 0) {
-        return(data)
-    }
-    temp_data <- as.data.frame(data)
-    cols <- which(tolower(names(data)) %in% c(
-        "scientificname",
-        "taxonrank",
-        "eventdate",
-        "country",
-        "decimallatitude",
-        "decimallongitude",
-        "latitude",
-        "longitute"
-    ))
-    
-    cols <- c(cols, setdiff(1:length(data), cols)); 
-    temp_data <- data[, cols] 
-    hiding_cols <- c()
-    temp_data[] <- lapply(temp_data, as.character)
-    
-    for (i in 1:length(names(temp_data))) {
-        size <- ifelse(nrow(temp_data) > 1000, 1000, nrow(temp_data))
-        sample <-
-            sample(1:nrow(temp_data), size = size)
-        f <-
-            mean(sapply(temp_data[sample, i], function(x)
-                nchar(x)), na.rm = T)
-        
-        if (!is.nan(f)) {
-            if (f > 50) {
-                hiding_cols <- c(hiding_cols, i)
-            }
-        }
-    }
-    
-    if (length(hiding_cols) > 0) {
-        temp_data <- temp_data[, c(hiding_cols * -1)]
-    }
-    temp_data
-}
-
-
-#' @title   summarize biodiversity dataframe
-#' @description  Summary table function.
-#'
-#' @param data biodiversity dataframe
+#' @param dataframe bddwc dictionary
 #' 
 #' @importFrom utils capture.output write.table
 #'
@@ -68,13 +18,4 @@ get_edit_string <- function(dataframe){
             row.names = F
         )
     ), collapse = "\n "))
-}
-
-#' @export
-return_core <- function(reactiveObject){
-    if(class(reactiveObject) == "reactive" || class(reactiveObject) == "reactiveExpr"){
-        return(reactiveObject())
-    } else {
-        return(reactiveObject)
-    }
 }
